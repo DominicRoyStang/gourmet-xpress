15fff0fc584ed24f9814da97e1294dc0dec24d3f
Karen Lin <karenxlin@gmail.com>
2017-10-14 19:46:43 -0400

    select query for orders

--- assets/db/clientOrder.sql
+++ assets/db/clientOrder.sql
@@ -4,9 +4,16 @@ CREATE TABLE IF NOT EXISTS ClientOrder (
     userId int NOT NULL,
     mealId int NOT NULL,
     productType int NOT NULL,
-    orderdate TIMESTAMP NOT NULL,
+    orderDate TIMESTAMP NOT NULL,
 	
     FOREIGN KEY (userId) REFERENCES Customer(userId),
     FOREIGN KEY (mealId) REFERENCES Meal(mealId),
     FOREIGN KEY (productType) REFERENCES Products(productType)
 )
+
+
+SELECT productType, mealId, COUNT(*)
+FROM ClientOrder o
+JOIN Meal m ON m.mealId = o.mealId
+WHERE DATE(orderDate) = DATE($today)
+GROUP BY productType, mealId 
